42636e921e8e71f95dc7ae550ccf576f797555cb
kshhyun <tjgus8288508@gmail.com>
2024-04-16 16:59:43 +0900

    add project_upload.js

---
+++ templates/js/project_upload.js
@@ -0,0 +1,7 @@
+function openTextInput() {
+    document.getElementById('textInput').style.display = 'block';
+}
+
+function handleFileSelect(event) {
+    var files = event.target.files; // 선택된 파일 가져오기
+}
